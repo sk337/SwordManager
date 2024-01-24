c00d0d408a9656eb9e318492fedf301f783830ce
sk337 <nathan@shorthoppers.com>
2024-01-23 16:03:03 -0800

    update nav redirects to work

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,22 +7,36 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+} from "@/components/ui/navigation-menu";
 
-
-export default function Nav(){
+export default function Nav() {
   return (
     <NavigationMenu className="m-5 text-center">
       <NavigationMenuList>
         <NavigationMenuItem>
-          <a href="#">Home</a>
+          <a
+            href="#"
+            onClick={() => {
+              window.location.hash = "#";
+              window.location.reload();
+            }}
+          >
+            Home
+          </a>
         </NavigationMenuItem>
         <NavigationMenuItem>|</NavigationMenuItem>
         <NavigationMenuItem>
-          <a href="#leaderboard">Leaderboard</a>
+          <a
+            href="#"
+            onClick={() => {
+              window.location.hash = "#leaderboard";
+              window.location.reload();
+            }}
+          >
+            Leaderboard
+          </a>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
-  )
-
+  );
 }
