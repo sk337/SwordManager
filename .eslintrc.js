fdfdc490d5449b7d538982e3d23b46a5e55dae8c
sk337 <nathan@shorthoppers.com>
2024-01-26 18:19:31 +0000

    replace tsx with jsx and add open graph generator

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "react"],
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-  },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
 };
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import path from "path";
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
-import UnpluginInjectPreload from 'unplugin-inject-preload/vite'
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
+import VitePluginBuildMetadata from 'vite-plugin-build-metadata';
+import ogPlugin from 'vite-plugin-open-graph';
+
 dotenv.config();
 
 
@@ -14,7 +17,30 @@ export default defineConfig({
         plugins: ["@babel/plugin-syntax-import-attributes"]
       }
     }),
-    // UnpluginInjectPreload({ /* options */ })
+    UnpluginInjectPreload({
+      files: [{
+        entryMatch: /.*\.ttf/
+      }]
+    }),
+    VitePluginBuildMetadata(),
+    ogPlugin({
+      basic: {
+        title: "Sword Manager",
+        type: "website",
+        url: process.env.VITE_URL || "http://sword-manager.pages.dev",
+        image: {
+          url: "./src/swordlogo.png",
+          type: "image/png",
+          width: 300,
+          height: 300,
+          alt: "Sword with Gear"
+        },
+        description: "100% client sided Sword Battle account manager written in react"
+      },
+      twitter: {
+        
+      }
+    })
   ],
   base: process.env.VITE_BASE_URL || "/",
   server: {
